a29d80181541d4752153b8e12fc0c9dfdd1854a9
semsag <sebassaenz304@gmail.com>
2025-03-29 23:42:41 -0500

    modificacion

--- registro.js
+++ registro.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const registro = async (req, res) => { //req = request, que es una petición; res = response, respuesta
     if (!req.session.usuario) {
-        res.status(401).send('No autorizado')
+        res.status(401).send('No autorizadoss')
         return
     }
     const datos = req.query;
